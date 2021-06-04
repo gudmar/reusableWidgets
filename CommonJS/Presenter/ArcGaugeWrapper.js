e934b999f16b7ac30b0ff1cc9abbd54f60fedf7d
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-06-05 00:35:15 +0200

    Refactored, need debugging

---
+++ CommonJS/Presenter/ArcGaugeWrapper.js
@@ -0,0 +1,52 @@
+class ArcGaugeWrapper extends PresenterMethodProvider{
+    constructor(context, subtype){
+        super();
+        this.elementSubtype = subtype;
+        this.context = context;
+        this.addWrappedElement('wrapped-element-id');
+        this.wrappedElement = this.context.shadowRoot.querySelector('#wrapped-element-id')
+        this.context.changePresenterElementSize('big')
+    }
+
+    addEvents(){
+        let controller = this.context.shadowRoot.querySelector('#wrapped-element-controller-id');
+        controller.addEventListener('linear-gauge-changed-value', this.changeWrappedElementValue.bind(this));
+        this.wrappedElement.addEventListener('gauge-changed-value', this.changeTargetElementsValue.bind(this, controller))
+    }
+
+    getElementSpecyficStyling(){
+        return`
+            <styling>
+
+            </styling>
+        `
+    }
+    getElementSpecyficTemplate(){
+        if (this.elementSubtype == 'degree-gauge'){
+            return `
+                <degree-gauge id = "wrapped-element-id"  data-label = "angle-gauge" data-approximate='1'></degree-gauge>
+                <line-gauge id = "wrapped-element-controller-id" data-label = "Gauge controller" data-size= "150" data-color-theme = "blue" data-min-val = "0", data-max-val = "360", data-value = "1"></line-gauge>
+            `
+        }
+        if (this.elementSubtype == 'percentage-gauge'){
+            return `
+                <percentage-gauge id = "wrapped-element-id" data-value="85" data-label = "percentage-gauge" data-approximate='0'></percentage-gauge>
+                <line-gauge id = "wrapped-element-controller-id" data-label = "Gauge controller" data-size= "150" data-color-theme = "blue" data-min-val = "0", data-max-val = "100", data-value = "85"></line-gauge>
+            `
+        }
+        if (this.elementSubtype == 'speed-gauge'){
+            return `
+                <speed-gauge id = "wrapped-element-id" data-value = "105" data-label = "Speedmeter" data-approximate='0'></speed-gauge>
+                <line-gauge id = "wrapped-element-controller-id" data-label = "Gauge controller" data-size= "150" data-color-theme = "blue" data-min-val = "0", data-max-val = "0", data-value = "385"></line-gauge>
+            `
+        }
+    }
+
+    getMenuContentAsStirng(){
+        return `
+        <div id="colorChoserId" data-label-set="blue,green,red">
+            Option stylig is not supported by this component, just this id='colorChoserId' element is significant to avoid error
+        </div>
+        `
+    }
+}
---
+++ CommonJS/Presenter/WaitingCircleWrapper.js
@@ -0,0 +1,71 @@
+class WaitnigCircleWrapper extends PresenterMethodProvider{
+    constructor(context, elementSubtype){
+        super();
+        this.wrappedElementSubtype = elementSubtype;
+        this.context = context;
+        this.addWrappedElement('wrapped-element-id');
+        this.wrappedElement = this.context.shadowRoot.querySelector('#wrapped-element-id')
+
+        this.colorChoser = this.context.shadowRoot.querySelector('#colorChoserId')
+        this.sizeChoser = this.context.shadowRoot.querySelector('#sizeChoserId')
+        this.optionsMenu = this.context.shadowRoot.querySelector('.options');
+        this.optionsMenuCloseButton = this.context.shadowRoot.querySelector('.close-button');
+        this.addOnclickWrappedElement()
+
+    }
+
+    addOnclickWrappedElement(){
+        this.context.setAttribute('onclick', `WaitnigCircleWrapper.openModalWithContent('waiting-circle', '${this.wrappedElementSubtype}')`)
+    }
+
+
+    // static openModalWithContent(elementType, elementSubtype) {
+    //     let content = null;
+    //     let modal = document.createElement('killable-modal');
+    //     content = new CodePresentationCustomWebElement(WaitngCircleDetailsDB.getDetailsAbout(elementSubtype))
+    //     modal.insertElementToKillableModal(content);
+    //     document.querySelector('body').appendChild(modal)
+    // }
+
+    static openModalWithContent(elementSubtype){
+        let modal = document.createElement('killable-modal');
+        let content = new CodePresentationCustomWebElement(WaitngCircleDetailsDB.getDetailsAbout(elementSubtype))
+        console.log(elementSubtype)
+        modal.insertElementToKillableModal(content);
+        document.querySelector('body').appendChild(modal)
+    }
+
+    addEvents(){
+        this.sizeChoser.addEventListener('click', this.setSize.bind(this))
+        this.colorChoser.addEventListener('click', this.setColorTheme.bind(this))
+    }
+
+    setSize() {
+        let size = this.sizeChoser.getAttribute('data-position');
+        this.wrappedElement.setAttribute('data-size', size)
+        this.context.changePresenterElementSize(size)
+    }
+
+
+    getElementSpecyficStyling(){
+        return`
+            <styling>
+
+            </styling>
+        `
+    }
+    getElementSpecyficTemplate(){
+        return `
+        <waiting-circle id = "wrapped-element-id" data-element-subtype = '${this.wrappedElementSubtype}' data-color-theme = 'blue' data-size = 'small'></waiting-circle>
+        `
+    }
+
+    getMenuContentAsStirng(){
+        return `
+            <multi-switch id="colorChoserId" data-label-set="blue,green,gray"></multi-switch>
+            <multi-switch id="sizeChoserId" data-label-set="small,medium,big"></multi-switch>
+        `
+    }
+
+
+}
