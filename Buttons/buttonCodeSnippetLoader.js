65a00cd3c47538c4e7e3cd6702fe376dd4553dfb
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-07-08 19:39:41 +0200

    wobble button code snippet added

--- Buttons/buttonCodeSnippetLoader.js
+++ Buttons/buttonCodeSnippetLoader.js
@@ -689,6 +689,120 @@ class ButtonDetailsDB{
                 `
             },
 
+            'wobble-horizontal-button': {
+                doNotWrapp: true,
+                innerCode: `
+                <p>This button is based on article showing how to imitate neons with css: 
+                    <a href = "https://css-tricks.com/how-to-create-neon-text-with-css/">link</a>, so for exploanation why there 
+                    are that many shadows needed and why keyframes are used please refer to source article.
+                </p>
+                <pre>
+                *{
+                    position: relative;
+                }
+                .button-wrapper{
+                    display: inline-block
+                }
+                .button-big{
+                    --button-font-size: 1.5rem;
+                    --button-padding: 10px;
+                }
+                .button-small{
+                    --button-font-size: 1rem;
+                    --button-padding: 5px;
+                }
+    
+                .color-theme-blue{
+                    --button-bg: blue;
+                    --button-fg: white;
+                    --button-hover-bg: rgb(120, 120, 255);
+                    --button-hover-fg: white;
+                    --button-active-bg: rgb(200, 200, 255);
+                    --button-active-fg: black;
+                    --button-border-color: rgba(0, 0, 0, 0);
+                }
+    
+    
+                .button{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    display: flex;
+                    text-align: center;
+                    color: var(--button-fg);
+                    background-color: var(--button-bg);
+                    font-family: Arial;
+                    border: solid thin var(--button-border-color);
+                    border-radius: 5px;
+                    padding: var(--button-padding);
+                }
+    
+                .color-theme-inactive:hover{
+                    cursor: not-allowed;
+                }
+    
+                .tooltip {
+                    position: absolute;
+                    background: white;
+                    color: black;
+                    border-radius: 5px;
+                    padding: 5px;
+                    max-width: 100px;
+                    line-break: anywhere;
+                    z-index: 70;
+                }
+    
+    
+                .wobble-horizontal-button{
+                    transition: 0.5s;
+                }
+    
+                .wobble-horizontal-button:hover {
+                 animation: wobble-horizontal 0.7s linear;
+                }
+                @keyframes wobble-horizontal {
+                    20% {transform: translate(30px, 0px);}
+                    40% {transform: translate(-50px, 0px);}
+                    60% {transform: translate(35px, 0px);}
+                    80% {transform: translate(-20px, 0px);}
+                    100% {transform: translate(5px, 0px);}
+                }
+                .wobble-horizontal-button{
+                    transition: 0.5s;
+                }
+    
+                .button-wrapper:hover>
+                .wobble-horizontal-button:after{
+                    content: '';
+                    position: absolute;
+                    width: 350%;
+                    height: 150%;
+                    z-index: 30;
+                }
+    
+    
+    
+                .shutter{
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                    width: 100%;
+                    width: 100%;
+                    height: 100%;
+                    margin:0;
+                    background-color: rgba(250, 250, 250, 0.5);
+                    transform: scaleY(0);
+                    transition: 0.3s;
+                }
+                .wobble-horizontal-button:active > .shutter{
+                    transform: scaleY(1);
+                    transition-timing-function: cubic-bezier(.8,2,0,0);
+                }
+    
+                </pre>
+                `
+            },
+
             'dark-sample-button': {
                 doNotWrapp: true,
                 innerCode: `
