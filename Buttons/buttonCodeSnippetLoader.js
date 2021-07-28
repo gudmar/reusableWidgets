1d31bb7cc43d0c6fbef028b95cc41dde9e270304
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-07-28 22:12:20 +0200

    bounce top button code snippet added

--- Buttons/buttonCodeSnippetLoader.js
+++ Buttons/buttonCodeSnippetLoader.js
@@ -1323,6 +1323,8 @@ class ButtonDetailsDB{
                 `
             },
 
+            
+
 
             'sweep-to-left-button': {
                 doNotWrapp: true,
@@ -1424,6 +1426,56 @@ class ButtonDetailsDB{
                 `
             },
 
+            'sweep-top-button': {
+                doNotWrapp: true,
+                innerCode: `
+                <h3>CSS:</h3>
+                <pre>
+
+                .sweep-top-button {
+                    transition: 0.3s;
+                    transition-property: color;
+                    position:relative;  /* without this whole screan is animated*/
+                }
+                .sweep-top-button:before {
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    transform: scaleY(0);
+                    color: var(--button-color-focus);
+                    background-color: var(--button-fg);
+                    transition-property: transform;
+                    transition-duration: 0.3s;
+                    transform-origin: top;
+                    content: "";
+                    z-index: -1;
+                }
+                .sweep-top-button:hover, .sweep-top-button:focus, .sweep-top-button:active {
+                    color: var(--button-hover-fg);
+                }
+                
+                .sweep-top-button:hover:before, .sweep-top-button:focus:before, .sweep-top-button:active:before {
+                    transform: scaleY(1);
+                }
+                .sweep-top-button {   
+                    z-index: 1;
+                }
+    
+                </pre>
+                <h3>HTML</h3>
+                <pre>
+&lt;div class = "button-wrapper">
+    &lt;div class="button color-theme-blue sweep-top-button   button-big" >
+        &lt;div class = "shutter"></div>
+            &lt;span>&lt;/span>
+        &lt;/div>
+    &lt;/div>                
+                </pre>
+                `
+            },
+
 
             'sweep-to-top-button': {
                 doNotWrapp: true,
@@ -1447,7 +1499,7 @@ class ButtonDetailsDB{
                     background-color: var(--button-fg);
                     transition-property: transform;
                     transition-duration: 0.3s;
-                    transform-origin: top;
+                    transform-origin: bottom;
                     content: "";
                     z-index: -1;
                 }
